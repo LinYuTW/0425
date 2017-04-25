2639bcf0b4567872c50ada95bc7b37ba17134ebb
JANE-YU,LIN <dodo_edward_lin@yahoo.com.tw>
2017-04-25 20:08:25 +0800

    Signed-off-by: JANE-YU,LIN <dodo_edward_lin@yahoo.com.tw>

--- ch7/計算總分/計算總分/Form1.vb
+++ ch7/計算總分/計算總分/Form1.vb
@@ -1,11 +1,15 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
         Dim sum As Integer = 0
-        Dim score() = {90, 90, 90, 80, 80, 80}
-        For i = 0 To 5
+        Dim score() = {95, 75, 80, 69, 87, 59, 43, 8, 97, 86, 73, 90}
+        Array.Sort(score)
+        For i = 0 To 11 Step 1
             sum = sum + score(i)
             TextBox1.Text = TextBox1.Text & "score(" & i & ")=" & score(i) & vbNewLine
             TextBox1.Text = TextBox1.Text & "sum=" & sum & vbNewLine
         Next
+        sum = sum / score.Length
+        TextBox1.Text = TextBox1.Text & "總共：" & score.Length & " 位" & vbNewLine
+        TextBox1.Text = TextBox1.Text & "平均：" & sum
     End Sub
 End Class
